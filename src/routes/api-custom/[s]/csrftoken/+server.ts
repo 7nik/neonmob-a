6a17e6d8a6285d3dd490869707f287af00fe2acc
7nik <kifiranet@gmail.com>
2023-07-17 21:32:21 +0300

    fix env?

--- src/routes/api-custom/[s]/csrftoken/+server.ts
+++ src/routes/api-custom/[s]/csrftoken/+server.ts
@@ -1,13 +1,13 @@
-import { HOSTNAMES } from "$env/static/private";
+// import { HOSTNAMES } from "$env/static/private";
 import { error, json } from "@sveltejs/kit";
 import domain from "../domain.js";
 
-const allowedHosts = HOSTNAMES.split(",");
+// const allowedHosts = HOSTNAMES.split(",");
 
 export const GET = async ({ request, params, cookies }) => {
-    if (!allowedHosts.includes(request.headers.get("host")!)) {
-        throw error(403);
-    }
+    // if (!allowedHosts.includes(request.headers.get("host")!)) {
+    //     throw error(403);
+    // }
     const resp = await fetch(`${domain(params.s)}/login`);
     for (const [name, value] of resp.headers.entries()) {
         if (name.toLowerCase() === "set-cookie" && value.startsWith("csrftoken=")) {
